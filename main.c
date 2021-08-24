1b0624995cbce3dc6ecba774255e4e53ee160084
Daksh Bardoliwala <88694228+Daksh-Bardoliwala@users.noreply.github.com>
2021-08-24 16:30:17 +0000

    Changed the code

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
+char *encode(char text[]) {
+  int len = strlen(text);
+  char *result;
+  for (int i = 0; i < len; i++) {
+    result[i] = (char) ((int) text[i] + 1);
+  }
+  return result;
+}
+
+// Encoding - Decoding
 int main() {
-  printf("Hello World\n");
+  char *text = encode("HI");
+  printf("%s", text);
+  return 0;
 }
