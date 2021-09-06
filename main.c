614282abfcb4bf89686c4e8f028fc6139b7161eb
Daksh Bardoliwala <88694228+Daksh-Bardoliwala@users.noreply.github.com>
2021-09-06 01:36:59 +0000

    New code

--- main.c
+++ main.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
-#include <string.h>
 
-char *encode(char text[]) {
-  int len = strlen(text);
-  char *result;
-  for (int i = 0; i < len; i++) {
-    result[i] = (char) ((int) text[i] + 1);
+int main(void) {
+  char s[5] = "TEST";
+  for (int i = 0; i < 4; i++) {
+    printf("%p\n", &s[i]);
   }
-  return result;
-}
-
-// Encoding - Decoding
-int main() {
-  char *text = encode("HI");
-  printf("%s", text);
   return 0;
 }
